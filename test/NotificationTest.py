9367f70af2f521808434e0b1db0d134480d63d00
jadalaou <jadalaou@eurecom.fr>
2024-07-18 11:10:44 +0200

    fixed failure cases

--- test/NotificationTest.py
+++ test/NotificationTest.py
@@ -1,8 +1,6 @@
 import os
 import sys
 import logging
-import multiprocessing
-import time
 import yaml
 from pymongo import MongoClient
 import importlib
@@ -19,6 +17,21 @@ stop_handler = init_handler.stop_handler
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def mongo_access(service_type: str):
+    try:
+        logging.getLogger('pymongo').setLevel(logging.INFO)
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['notification_db']
+        if service_type == "amf":
+            return db['amf_notifications']
+        elif service_type == "smf":
+            return db['smf_notifications']
+        else:
+            raise ValueError(f"Invalid service type: {service_type}")
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        sys.exit(-1) 
+               
 def extract_imsi_from_docker_yaml(docker_yaml_path):
     with open(docker_yaml_path, 'r') as file:
         data = yaml.safe_load(file)
@@ -38,32 +51,8 @@ def extract_imsi_from_docker_yaml(docker_yaml_path):
         logger.error("No IMSIs found in Docker YAML file")
         sys.exit(-1)
 
-def get_imsi_from_handler_collection():
-    try:
-        logging.getLogger('pymongo').setLevel(logging.INFO)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['notification_db']
-        amf_collection = db['amf_notifications']
-        latest_imsi_events = {}
-        for document in amf_collection.find():
-            for report in document["reportList"]:
-                supi = report["supi"]
-                rm_state = report["rmInfoList"][0]["rmState"]
-                timestamp = report["timeStamp"]
-                if supi.startswith("imsi-"):
-                    supi = supi[5:]
-                if supi not in latest_imsi_events or timestamp > latest_imsi_events[supi]['timestamp']:
-                    latest_imsi_events[supi] = {'rm_state': rm_state, 'timestamp': timestamp}
-        latest_registered_imsis = [
-            imsi for imsi, event in latest_imsi_events.items()
-            if event['rm_state'] == "REGISTERED"]
-        return latest_registered_imsis
-    except Exception as e:
-        logger.error(f"Failed to get IMSIs from handler collection: {e}")
-        stop_handler()
-        sys.exit(-1)
-        
 def check_smf_logs_and_callback_notification(logs):
+   
     try:
         smf_contexts = re.findall(r'SMF CONTEXT:.*?(?=SMF CONTEXT:|$)', logs, re.DOTALL)
         parsed_log_data = []
@@ -83,10 +72,8 @@ def check_smf_logs_and_callback_notification(logs):
                 if "PDN type:" in line:
                     parsed_context['PDN type'] = line.split(':')[1].strip()
             parsed_log_data.append(parsed_context)
-        logging.getLogger('pymongo').setLevel(logging.INFO)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['notification_db']
-        smf_collection = db['smf_notifications']
+
+        smf_collection = mongo_access("smf")
         callback_data = []
 
         for document in smf_collection.find():
@@ -115,18 +102,41 @@ def check_smf_logs_and_callback_notification(logs):
                         break
                 if not match_found:
                     logger.error(f"Mismatch found for SUPI: {log_entry['SUPI']}")
-                    sys.exit(-1)
+                    raise Exception(f"Mismatch found for SUPI: {log_entry['SUPI']}")
 
             logger.info("All SMF contexts match the callback data.")
 
         else :
 
-            logger.error(f"No SMF contexts found in logs.{type(logs)}")
-            sys.exit(-1)
+            logger.error(f"No SMF contexts found in logs.")
+            raise Exception("No SMF contexts found in logs.")
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
-        sys.exit(-1)
+        raise e
+    
+          
+def get_imsi_from_handler_collection():
+    try:
+        amf_collection = mongo_access("amf")
+        latest_imsi_events = {}
+        for document in amf_collection.find():
+            for report in document["reportList"]:
+                supi = report["supi"]
+                rm_state = report["rmInfoList"][0]["rmState"]
+                timestamp = report["timeStamp"]
+                if supi.startswith("imsi-"):
+                    supi = supi[5:]
+                if supi not in latest_imsi_events or timestamp > latest_imsi_events[supi]['timestamp']:
+                    latest_imsi_events[supi] = {'rm_state': rm_state, 'timestamp': timestamp}
+        latest_registered_imsis = [
+            imsi for imsi, event in latest_imsi_events.items()
+            if event['rm_state'] == "REGISTERED"]
+        return latest_registered_imsis
+    except Exception as e:
+        logger.error(f"Failed to get IMSIs from handler collection: {e}")
+        stop_handler()
+        raise e
 
 def check_imsi_match(docker_yaml_path,nb_of_users):
     imsi_from_yaml = extract_imsi_from_docker_yaml(docker_yaml_path)[:nb_of_users]
@@ -137,15 +147,12 @@ def check_imsi_match(docker_yaml_path,nb_of_users):
         logger.error(f"IMSI mismatch. Docker YAML IMSI: {imsi_from_yaml}, Handler IMSI: {imsi_from_handler}")
         remove_ues(nb_of_users)
         stop_handler()
-        sys.exit(-1)
+        raise Exception("IMSI mismatch.")
         
         
 def check_latest_deregistered_imsis(docker_yaml_path, n):
     try:
-        logging.getLogger('pymongo').setLevel(logging.INFO)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client['notification_db']
-        amf_collection = db['amf_notifications']
+        amf_collection = mongo_access("amf")
         imsis_from_yaml = extract_imsi_from_docker_yaml(docker_yaml_path)[:n]      
         latest_deregistered_imsis = []
         
@@ -165,10 +172,10 @@ def check_latest_deregistered_imsis(docker_yaml_path, n):
             logger.info("Deregistered IMSI match successful.")
         else:
             logger.error(f"Deregistered IMSI mismatch. Docker YAML IMSIs: {imsis_from_yaml}, Deregistered IMSIs: {latest_deregistered_imsis}")
-            sys.exit(-1)
+            raise ValueError("Deregistered IMSI mismatch.")
     except Exception as e:
         logger.error(f"Failed to check latest deregistered IMSIs: {e}")
-        sys.exit(-1)
+        raise e
 
 def add_ues_process():
     try:
@@ -178,49 +185,3 @@ def add_ues_process():
         logger.error(f"Core network is not healthy. UEs were not added: {e}")
         stop_handler()
         sys.exit(-1)
-
-if __name__ == "__main__":
-    add_ues_process()
-    time.sleep(30)
-    remove_ues(1)
-    try:
-        start_handler()
-    except Exception as e:
-        logger.error(f"Failed to start handler: {e}")
-        sys.exit(1)
-
-    # Give the handler some time to start
-    time.sleep(20)
-    nb_of_users = 1
-    for user in range(nb_of_users):
-        try:
-            ues_proc = multiprocessing.Process(target=add_ues_process)
-            ues_proc.start()
-            ues_proc.join()
-            time.sleep(5)
-        except Exception as e:
-            logger.error(f"Failed to add UEs: {e}")
-            sys.exit(-1)
-        
-    time.sleep(10)
-    docker_yaml_path = os.path.join(parent_dir, '5g_rfsimulator', 'docker-compose.yaml')
-    check_imsi_match(docker_yaml_path, nb_of_users)
-
-    time.sleep(3)
-    try:
-        remove_ues(nb_of_users)
-    except Exception as e:
-        logger.error(f"Failed to remove UEs: {e}")
-        sys.exit(-1)
-    time.sleep(15)
-    check_latest_deregistered_imsis(docker_yaml_path, nb_of_users)
-    
-    logger.info("Stopping handler...")
-    try:
-        stop_handler()
-    except Exception as e:
-        logger.error(f"Failed to stop handler: {e}")
-        sys.exit(-1)
-        
-    logger.info("Handler stopped.")
-    sys.exit(0)
