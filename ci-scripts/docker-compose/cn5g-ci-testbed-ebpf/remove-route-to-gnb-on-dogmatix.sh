d4da1f6f2a834e7d3c451db459161cea6445b4e0
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2024-12-09 11:54:59 +0100

    chore(ci): initial UPF-eBPF E2E Test with separate hosts
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

---
+++ ci-scripts/docker-compose/cn5g-ci-testbed-ebpf/remove-route-to-gnb-on-dogmatix.sh
@@ -0,0 +1,2 @@
+sudo ip route del 192.168.70.0/26 via 172.21.19.40 dev bond0
+sudo ip route del 192.168.71.0/26 via 172.21.19.40 dev bond0
---
+++ ci-scripts/docker-compose/gnb-ci-testbed-ebpf/remove-route-to-cn5g-on-selfix.sh
@@ -0,0 +1,2 @@
+sudo ip route del 192.168.70.128/26 via 172.21.16.101 dev enp3s0
+sudo ip route del 192.168.71.128/26 via 172.21.16.101 dev enp3s0
